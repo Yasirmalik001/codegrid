e53d4cd2a2207db28cd3a955f105d531c161d4a6
YASIR MALIK <148976425+Yasirmalik001@users.noreply.github.com>
2024-10-09 13:16:44 +0500

    chnages push

--- cg-we-go-again/script.js
+++ cg-we-go-again/script.js
@@ -1,24 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let e = document.querySelectorAll(".menu-item");
-  e.forEach((e) => {
-    let t = e.children[0].innerText.trim().split(/\s+/),
-      n = "";
-    t.forEach((e) => {
-      let t = "";
-      e.split("").forEach((e, n) => {
-        t += `<span style="--index: ${n};">${e}</span>`;
-      }),
-        (n += `${t}&nbsp;`);
-    }),
-      (e.children[0].innerHTML = n);
-    let r = e.children[0].cloneNode(!0);
-    (r.style.position = "absolute"),
-      (r.style.left = "0"),
-      (r.style.top = "0"),
-      e.appendChild(r);
-  });
-});
-
 import { galleryItems } from "./data.js";
 
 document.addEventListener("DOMContentLoaded", function () {
